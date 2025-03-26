5583f6804e01a25e42bff3f578c2fd1c0651a1ae
Fernando <fdofuentescarrasco@gmail.com>
2025-03-26 12:52:14 +0100

    Doing it faster

--- fraudDetection/SVM.py
+++ fraudDetection/SVM.py
@@ -1,7 +1,7 @@
 import kagglehub
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report, confusion_matrix, roc_curve, auc
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report, confusion_matrix
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
@@ -130,19 +130,3 @@ print(confusion_matrix(y_train, y_train_pred))
 print("\nConfusion Matrix (Test):")
 print(confusion_matrix(y_test, y_test_pred))
 
-# ROC Curve - Plotting and AUC Calculation
-y_probs = svc_model.predict_proba(X_test_scaled_pca)[:, 1]
-fpr, tpr, thresholds = roc_curve(y_test, y_probs)
-roc_auc = auc(fpr, tpr)
-
-# Plot the ROC curve
-plt.figure(figsize=(8, 6))
-plt.plot(fpr, tpr, color='darkorange', lw=2, label=f'ROC curve (AUC = {roc_auc:.4f})')
-plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
-plt.xlim([0.0, 1.0])
-plt.ylim([0.0, 1.05])
-plt.xlabel('False Positive Rate')
-plt.ylabel('True Positive Rate (Recall)')
-plt.title('Receiver Operating Characteristic (ROC) Curve')
-plt.legend(loc='lower right')
-plt.show()
